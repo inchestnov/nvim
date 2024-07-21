9efffd15c790145e02d5c617b9385ba0d2f48072
Igor Chestnov <inchestnov@gmail.com>
2024-07-21 23:32:37 +0300

    remove deprecated keymapping

--- lua/inchestnov/core/keymaps.lua
+++ lua/inchestnov/core/keymaps.lua
@@ -10,10 +10,6 @@ vim.keymap.set("n", "<leader>e", "<cmd>close<CR>", { desc = "Close current split
 vim.keymap.set("v", "<C-Down>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-Up>", ":m '<-2<CR>gv=gv")
 
--- increment/decrement numbers
-vim.keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
-vim.keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
-
 -- splits
 vim.keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
 vim.keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
@@ -26,11 +22,6 @@ vim.keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current t
 vim.keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
 vim.keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
 vim.keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
-
--- dap
-vim.keymap.set("n", "<leader>db", '<cmd>lua require("dap").toggle_breakpoint()<CR>', { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>dc", '<cmd>lua require("dap").continue()<CR>', { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>dn", '<cmd>lua require("dap").step_over()<CR>', { noremap = true, silent = true })
 
 -- Format code using LSP
 vim.keymap.set("n", "<leader>ff", vim.lsp.buf.format)
