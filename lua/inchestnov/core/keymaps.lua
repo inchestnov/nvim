26fefa6c5482bb91ef0c0a2776291450295c90e7
Igor Chestnov <ichestnov@ozon.ru>
2025-06-27 10:23:45 +0300

    split navigations keys

--- lua/inchestnov/core/keymaps.lua
+++ lua/inchestnov/core/keymaps.lua
@@ -2,4 +2,9 @@ vim.g.mapleader = " "
 
 vim.keymap.set("n", "-", ":Ex<CR>", { noremap = true, silent = true})
 
+vim.api.nvim_set_keymap("n", "<C-Left>",  "<C-w>h", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-Down>", "<C-w>j", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-Up>",    "<C-w>k", { silent = true })
+vim.api.nvim_set_keymap("n", "<C-Right>",  "<C-w>l", { silent = true })
+
 -- cooming soon...
