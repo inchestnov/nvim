00d188b61b7c6b0ac47a3e7b613139f035884d83
Igor Chestnov <ichestnov@ozon.ru>
2025-06-27 19:07:26 +0300

    blink

--- lua/inchestnov/core/lsp.lua
+++ lua/inchestnov/core/lsp.lua
@@ -1,10 +1,10 @@
 vim.lsp.enable({'luals', 'gopls'})
 
-vim.lsp.config('*', {
-  on_init = function(client)
-    print(client.name .. ' now runs in the background')
-  end,
-})
+-- vim.lsp.config('*', {
+--   on_init = function(client)
+--     print(client.name .. ' now runs in the background')
+--   end,
+-- })
 
 vim.diagnostic.config({
   virtual_lines = true,
